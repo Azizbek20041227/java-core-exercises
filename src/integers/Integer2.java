2c5cfbefb963b26c0e8c3e3dff4fd1811b79520b
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-14 14:14:01 +0500

    Integer2 add

--- src/integers/Integer2.java
+++ src/integers/Integer2.java
@@ -1,7 +1,17 @@
 package integers;
+import java.util.Scanner;
 
 public class Integer2 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter KG : ");
+        int KG = scanner.nextInt();
+
+        int T = 1000;
+        int Tonna = KG / T;
+
+        System.out.println("Tonna : " + Tonna);
     }
 }
