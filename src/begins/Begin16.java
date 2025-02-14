91c7897104e26df8bfaa65b4e5758b6771388ecd
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-14 14:04:00 +0500

    Begin16 add

---
+++ src/begins/Begin16.java
@@ -0,0 +1,18 @@
+package begins;
+import java.util.Scanner;
+
+public class Begin16 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter X1: ");
+        double X1 = scanner.nextDouble();
+        System.out.print("Enter X2: ");
+        double X2 = scanner.nextDouble();
+
+        double Masofasi = X2 - X1;
+
+        System.out.println("Masofasi : " + Masofasi);
+    }
+}
