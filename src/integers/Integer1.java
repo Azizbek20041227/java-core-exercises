49207f484aab82eb7f94aa2494d1a41e84d63a2a
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-14 14:12:22 +0500

    Integer1 add

--- src/integers/Integer1.java
+++ src/integers/Integer1.java
@@ -1,7 +1,17 @@
 package integers;
+import java.util.Scanner;
 
 public class Integer1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter SM : ");
+        int SM = scanner.nextInt();
+
+        int M = 1000;
+        int Metr = SM / M;
+
+        System.out.println("Metr : " + Metr);
     }
 }
