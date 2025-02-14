dd1da9f9cc43dcca748d385c646a3c70da73f0bf
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-14 11:03:18 +0500

    Begin2 add

--- src/begins/Begin2.java
+++ src/begins/Begin2.java
@@ -1,7 +1,16 @@
 package begins;
+import java.util.Scanner;
 
 public class Begin2 {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        double a = scanner.nextDouble();
+
+        double Sirti = a * a;
+
+        System.out.println("Sirti: " + Sirti);
 
     }
 }
