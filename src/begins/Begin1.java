470063aa9a074efb2dbf72fbbe8491bdf29ec8ac
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-14 10:59:31 +0500

    Begin1 add

--- src/begins/Begin1.java
+++ src/begins/Begin1.java
@@ -1,7 +1,15 @@
 package begins;
+import java.util.Scanner;
 
 public class Begin1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        double a = scanner.nextDouble();
+
+        double Perimeteri = 4 * a;
+
+        System.out.println("Perimeteri: " + Perimeteri);
 
     }
 }
