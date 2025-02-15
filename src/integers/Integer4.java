66a2478b4c4210fd718f30cac0331904faa32b91
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-15 14:02:08 +0500

    Integer4 add

---
+++ src/integers/Integer4.java
@@ -0,0 +1,18 @@
+package integers;
+import java.util.Scanner;
+
+public class Integer4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter A : ");
+        int A = scanner.nextInt();
+        System.out.print("Enter B : ");
+        int B = scanner.nextInt();
+
+        int Javob = A / B;
+
+        System.out.println("Javob : " + Javob);
+    }
+}
