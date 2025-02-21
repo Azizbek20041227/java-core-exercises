c7d309c64e8dcf4a468d8a10db9c778305a913b0
Azizbek <muhammadnazarovazizbek@gmail.com>
2025-02-21 09:51:52 +0500

    Matrix04 add

---
+++ src/matrixs/Matrix04.java
@@ -0,0 +1,32 @@
+package matrixs;
+import java.util.Scanner;
+
+public class Matrix04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("M ni kiritng: ");
+        int m = scanner.nextInt();
+        System.out.print("N ni kiriting: ");
+        int n = scanner.nextInt();
+
+        int[] uzunligi = new int[m];
+        System.out.println("M uzunlikdagi haqiqiy sonlarni kiriting: ");
+        for (int i = 0; i < m; i++) {
+            uzunligi[i] = scanner.nextInt();
+        }
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = uzunligi[i];
+            }
+        }
+        System.out.println("Hosil bolgan matritsa: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
